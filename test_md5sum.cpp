808965c1a735e70f9b148755ab9dcd4fab19ee36
SergeyKotus <KondrS@bk.ru>
2026-08-01 06:14:12 +0300

    2

--- test_md5sum.cpp
+++ test_md5sum.cpp
@@ -12,7 +12,7 @@ BOOST_AUTO_TEST_SUITE(test_md5sum)
 //}
 BOOST_AUTO_TEST_CASE(test_valid_md5sum)
 {
-    std::system("cat ip_filter.tsv | ip_filter | md5sum > readme.txt");
+    std::system("cat ip_filter.tsv | ./ip_filter | md5sum > readme.txt");
 
     std::string file_string = "";
     const std::string expected_string = "24e7a7b2270daee89c64d3ca5fb3da1a -";
@@ -22,9 +22,9 @@ BOOST_AUTO_TEST_CASE(test_valid_md5sum)
         std::getline(file, file_string);
         file.close();
     }
-    BOOST_CHECK(file_string.length() > 5);
+    //BOOST_CHECK(file_string.length() > 5);
 //    BOOST_TEST_MESSAGE(file_string.c_str());
-    //BOOST_CHECK(file_string == expected_string);
+    BOOST_CHECK(file_string == expected_string);
     //BOOST_CHECK(1 == 1);
     //BOOST_CHECK(std::strcmp(std::ifstream("readme.txt").rdbuf(), "24e7a7b2270daee89c64d3ca5fb3da1a") == 0);
     std::remove("readme.txt");
